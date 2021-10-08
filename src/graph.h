376c1e875efb4c855d8a77d62cc5dfbb6acaae51
andraspalasti <andraspalasti2@gmail.com>
2021-10-08 10:03:03 +0200

    added more docs

--- src/graph.h
+++ src/graph.h
@@ -12,11 +12,20 @@ typedef struct Graph {
 /*
 * Reads a graph from a csv file
 * The csv should look like this:
-* AA;10;10
-* OA;-678;-166
-* FD;31;392
+* AA;10;10;""
+* OA;-678;-166;""
+* FD;31;392;""
 */
 Graph *read_graph_from_file(char *fname);
+
+/*
+* Reads a graph from a csv file
+* The csv should look like this:
+* AA;10;10;""
+* OA;-678;-166;""
+* FD;31;392;""
+*/
+void *read_edges_from_file_to_graph(char *fname, Graph *g);
 
 /*
 * Adds a node to a graph
