8b981dde43a09b43031fdcc7ba33f43dbc69dab1
andraspalasti <andraspalasti2@gmail.com>
2021-10-30 15:41:00 +0200

    coordinate helper functions

--- src/coordinates.c
+++ src/coordinates.c
@@ -21,3 +21,62 @@ void print_coords(Coordinates coords) {
 double distance(Coordinates a, Coordinates b) {
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
+
+/**
+ * @brief Normalize coordinates into an all positive coordinate system
+ * 
+ * @param coords The coords to normalize
+ * @param width Width of avilable space
+ * @param height Height of avilable space
+ * @return Coordinates The normalized coordinates
+ */
+Coordinates normalize_coords(Coordinates coords, int width, int height) {
+    return (Coordinates){.x = coords.x + (double)width / 2.0, .y = coords.y + (double)height / 2.0};
+}
+
+/**
+ * @brief Scales the coordinates by the specified lambda
+ * 
+ * @param lambda The factor to scale with
+ * @param coords The coordinates to scale
+ * @return Coordinates The scaled coordinates
+ */
+Coordinates scale(double lambda, Coordinates coords) {
+    return (Coordinates){.x = lambda * coords.x, .y = lambda * coords.y};
+}
+
+/**
+ * @brief Rotates the specified coords around the specified origin
+ * 
+ * @param origin The origin to rotate around
+ * @param coords The coords to rotate
+ * @param alpha The angle to rotate by
+ * @return Coordinates The rotated coordinates
+ */
+Coordinates rotate_around(Coordinates origin, Coordinates coords, double alpha) {
+    Coordinates sub = subtract(coords, origin);
+    return (Coordinates){.x = sub.x * cos(alpha) - sub.y * sin(alpha) + origin.x,
+                         .y = sub.y * cos(alpha) + sub.x * sin(alpha) + origin.y};
+}
+
+/**
+ * @brief Sums the two coordinates
+ * 
+ * @param c1 
+ * @param c2 
+ * @return Coordinates 
+ */
+Coordinates add(Coordinates c1, Coordinates c2) {
+    return (Coordinates){.x = c1.x + c2.x, .y = c1.y + c2.y};
+}
+
+/**
+ * @brief Subtracts the two coordinates
+ * 
+ * @param c1 Coordinate to subtract from
+ * @param c2 
+ * @return Coordinates 
+ */
+Coordinates subtract(Coordinates c1, Coordinates c2) {
+    return (Coordinates){.x = c1.x - c2.x, .y = c1.y - c2.y};
+}
--- src/coordinates.h
+++ src/coordinates.h
@@ -1,6 +1,8 @@
 #ifndef COORDINATES_H
 #define COORDINATES_H
 
+#define PI 3.141592653589793
+
 typedef struct Coordinates {
     double x, y;
 } Coordinates;
@@ -8,5 +10,15 @@ typedef struct Coordinates {
 void print_coords(Coordinates coords);
 
 double distance(Coordinates a, Coordinates b);
+
+Coordinates normalize_coords(Coordinates coords, int width, int height);
+
+Coordinates scale(double lambda, Coordinates coords);
+
+Coordinates rotate_around(Coordinates origin, Coordinates coords, double alpha);
+
+Coordinates subtract(Coordinates c1, Coordinates c2);
+
+Coordinates add(Coordinates c1, Coordinates c2);
 
 #endif
