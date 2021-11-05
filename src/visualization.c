ed568e3cbc3ef59737c09a9b3f6d65f3eb7ba398
andraspalasti <andraspalasti2@gmail.com>
2021-11-05 20:00:03 +0100

    refactored code to use animation struct

--- src/visualization.c
+++ src/visualization.c
@@ -24,13 +24,10 @@ void display_graph(Graph *g) {
 
     SDL_RenderPresent(renderer);
 
-    Uint64 START = SDL_GetPerformanceCounter();
-    double delta_time = 0; // Time elapsed from start in milliseconds
-
-    const double ANIM_DURATION = 1500; // The duration of the animation in milliseconds
+    Animation *anim = init_animation(1500);
 
     SDL_Event e;
-    bool quit = false, is_finished = false;
+    bool quit = false;
     SDL_Color bg = hexcolor_to_sdl_color(BG_COLOR);
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
@@ -39,29 +36,21 @@ void display_graph(Graph *g) {
 
             // if the user presses 'r' then restart the animation
             if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_r) {
-                START = SDL_GetPerformanceCounter();
-                delta_time = 0;
-                is_finished = false;
+                restart_animation(anim);
             }
         }
 
         // if animation is over dont waste resources on drawing the same thing
         // rather check for input every 100ms
-        if (is_finished) {
-            // update elapsed time
-            delta_time = (double)((SDL_GetPerformanceCounter() - START) * 1000 / (double)SDL_GetPerformanceFrequency());
+        if (anim->is_finished == false) {
+
+            update_animation(anim);
 
             SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
             SDL_RenderClear(renderer);
 
-            // need to abstract this
-            if (ANIM_DURATION > delta_time)
-                draw_graph(renderer, font, g, NULL,
-                           cubic_bezier(0.8, 0.8, delta_time / ANIM_DURATION));
-            else {
-                draw_graph(renderer, font, g, NULL, 1.0);
-                is_finished = true;
-            }
+            draw_graph(renderer, font, g, NULL,
+                       anim->is_finished ? 1.0 : cubic_bezier(0.8, 0.8, anim->delta_time / anim->ANIM_DURATION));
 
             SDL_RenderPresent(renderer);
         } else {
@@ -69,6 +58,7 @@ void display_graph(Graph *g) {
         }
     }
 
+    free_animation(anim);
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
@@ -89,10 +79,7 @@ void display_graph_with_path(Graph *g, Path *p) {
     sdl_init(SCREEN_WIDTH, SCREEN_HEIGHT, &window, &renderer);
     TTF_Font *font = ttf_init("font.ttf", 14);
 
-    Uint64 START = SDL_GetPerformanceCounter();
-    double delta_time = 0; // Time elapsed from start in milliseconds
-
-    const double ANIM_DURATION = 1500; // The duration of the animation in milliseconds
+    Animation *anim = init_animation(1500);
 
     SDL_Event e;
     bool quit = false;
@@ -108,54 +95,60 @@ void display_graph_with_path(Graph *g, Path *p) {
                 quit = true;
 
             if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_r) {
-                START = SDL_GetPerformanceCounter();
-                delta_time = 0;
+                restart_animation(anim);
                 // reset animated nodes
                 from = p;
                 to = p != NULL ? p->next_node : NULL;
             }
         }
 
-        delta_time = (double)((SDL_GetPerformanceCounter() - START) * 1000 / (double)SDL_GetPerformanceFrequency());
+        if (anim->is_finished == false) {
+            update_animation(anim);
 
-        SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
-        SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, bg.r, bg.g, bg.b, bg.a);
+            SDL_RenderClear(renderer);
 
-        draw_graph(renderer, font, g, p, 1.0);
+            draw_graph(renderer, font, g, p, 1.0);
 
-        // draw the paths that finished animating
-        for (Path *i = p; i != from; i = i->next_node) {
-            if (is_connected(i->next_node->node, i->node)) {
-                draw_line_between_nodes(renderer, i->node->coords, i->next_node->node->coords, NODE_RADIUS, PATH_COLOR);
-            } else {
-                draw_arrow_between_nodes(renderer, i->node->coords, i->next_node->node->coords, NODE_RADIUS, PATH_COLOR);
+            // draw the paths that already finished animating
+            for (Path *i = p; i != from; i = i->next_node) {
+                if (is_connected(i->next_node->node, i->node)) {
+                    draw_line_between_nodes(renderer, i->node->coords, i->next_node->node->coords, NODE_RADIUS, PATH_COLOR);
+                } else {
+                    draw_arrow_between_nodes(renderer, i->node->coords, i->next_node->node->coords, NODE_RADIUS, PATH_COLOR);
+                }
             }
+
+            if (from != NULL && to != NULL) {
+                double scale_factor = anim->is_finished ? 1 : (anim->delta_time / anim->ANIM_DURATION);
+                if (is_connected(to->node, from->node)) {
+                    draw_line_between_nodes(renderer, from->node->coords,
+                                            add(scale(scale_factor, subtract(to->node->coords, from->node->coords)),
+                                                from->node->coords),
+                                            NODE_RADIUS, PATH_COLOR);
+                } else {
+                    draw_arrow_between_nodes(renderer, from->node->coords,
+                                             add(scale(scale_factor, subtract(to->node->coords, from->node->coords)),
+                                                 from->node->coords),
+                                             NODE_RADIUS, PATH_COLOR);
+                }
+
+                // restart animation and jump to next path to animate
+                if (anim->is_finished) {
+                    restart_animation(anim);
+                    from = to;
+                    to = to->next_node;
+                }
+            }
+
+            SDL_RenderPresent(renderer);
+
+        } else {
+            SDL_Delay(100);
         }
-
-        if (from != NULL && to != NULL) {
-            double scale_factor = ANIM_DURATION > delta_time ? (delta_time / ANIM_DURATION) : 1;
-            if (is_connected(to->node, from->node)) {
-                draw_line_between_nodes(renderer, from->node->coords,
-                                        add(scale(scale_factor, subtract(to->node->coords, from->node->coords)),
-                                            from->node->coords),
-                                        NODE_RADIUS, PATH_COLOR);
-            } else {
-                draw_arrow_between_nodes(renderer, from->node->coords,
-                                         add(scale(scale_factor, subtract(to->node->coords, from->node->coords)),
-                                             from->node->coords),
-                                         NODE_RADIUS, PATH_COLOR);
-            }
-            if (delta_time >= ANIM_DURATION) {
-                START = SDL_GetPerformanceCounter();
-                delta_time = 0;
-                from = to;
-                to = to->next_node;
-            }
-        }
-
-        SDL_RenderPresent(renderer);
     }
 
+    free_animation(anim);
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
