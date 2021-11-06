676281ffb17208c66f0b2ab66d7f25cb529deefe
andraspalasti <andraspalasti2@gmail.com>
2021-11-06 13:48:50 +0100

    rewrote draw functions

--- src/draw.c
+++ src/draw.c
@@ -10,15 +10,13 @@
 
 /**
  * @brief Scales the node's coordinates by scale_factor and draws the graph
- * It does not modify the graph
  * 
  * @param renderer 
  * @param font 
  * @param g 
  * @param exclude_path It will not draw these paths
- * @param scale_factor 
  */
-void draw_graph(SDL_Renderer *renderer, TTF_Font *font, Graph *g, Path *exclude_path, double scale_factor) {
+void draw_graph(SDL_Renderer *renderer, TTF_Font *font, const Graph *g, const Path *exclude_path) {
     // the matrix contains what edges we have already drawn
     bool **adjacency_matrix = init_bool_matrix(g->used, g->used);
     for (int i = 0; i < g->used; i++)
@@ -38,18 +36,12 @@ void draw_graph(SDL_Renderer *renderer, TTF_Font *font, Graph *g, Path *exclude_
         while (neighbour != NULL) {
             // only draw an edge if we have not drawn it yet
             if (adjacency_matrix[i][neighbour->node->idx] == false) {
-                // check if we can traverse edge both ways
-                // then we only draw one line
                 if (is_connected(neighbour->node, g->nodes[i])) {
-                    draw_line_between_nodes(renderer,
-                                            scale(scale_factor, g->nodes[i]->coords),
-                                            scale(scale_factor, neighbour->node->coords), NODE_RADIUS, EDGE_COLOR);
+                    draw_line_between_nodes(renderer, g->nodes[i], neighbour->node, NODE_RADIUS, EDGE_COLOR);
                     adjacency_matrix[i][neighbour->node->idx] = true;
                     adjacency_matrix[neighbour->node->idx][i] = true;
                 } else {
-                    draw_arrow_between_nodes(renderer,
-                                             scale(scale_factor, g->nodes[i]->coords),
-                                             scale(scale_factor, neighbour->node->coords), NODE_RADIUS, EDGE_COLOR);
+                    draw_arrow_between_nodes(renderer, g->nodes[i], neighbour->node, NODE_RADIUS, EDGE_COLOR);
                     adjacency_matrix[i][neighbour->node->idx] = true;
                 }
             }
@@ -68,71 +60,99 @@ void draw_graph(SDL_Renderer *renderer, TTF_Font *font, Graph *g, Path *exclude_
  * @brief Draws a line between the two nodes
  * 
  * @param renderer 
- * @param n1 Coordinates of node
- * @param n2 Coordinates of node
+ * @param n1 
+ * @param n2 
  */
-void draw_line_between_nodes(SDL_Renderer *renderer, Coordinates n1, Coordinates n2, double node_radius, Uint32 color) {
-    Coordinates c1 = normalize_coords(n1, SCREEN_WIDTH, SCREEN_HEIGHT);
-    Coordinates c2 = normalize_coords(n2, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    Coordinates sub = subtract(c2, c1);
-    double d = distance(c1, c2);
-    assert(d != 0); // we should not divide by zero
-
-    draw_line_between_coords(renderer,
-                             add(scale(node_radius / d, sub), c1),
-                             add(scale((d - node_radius) / d, sub), c1), color);
+void draw_line_between_nodes(SDL_Renderer *renderer, const Node *n1, const Node *n2, double node_radius, Uint32 color) {
+    Coordinates vector[2];
+    get_vector(vector, n1, n2, node_radius);
+    draw_line(renderer, vector[0], vector[1], color);
 }
 
 /**
  * @brief Draws an arrow between the two nodes
  * 
  * @param renderer 
- * @param n1 The node's coords that the arrow starts from
- * @param n2 The node's coords that the arrow points to
+ * @param n1 The node that the arrow starts from
+ * @param n2 The node that the arrow points to
  */
-void draw_arrow_between_nodes(SDL_Renderer *renderer, Coordinates n1, Coordinates n2, double node_radius, Uint32 color) {
-    Coordinates c1 = normalize_coords(n1, SCREEN_WIDTH, SCREEN_HEIGHT);
-    Coordinates c2 = normalize_coords(n2, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-    Coordinates sub = subtract(c2, c1);
-    double d = distance(c1, c2);
-    assert(d != 0); // we should not divide by zero
-
-    Coordinates arrow_head_start = add(scale((d - node_radius) / d, sub), c1);
-    Coordinates arrow_head_width = scale((d - node_radius - ARROW_HEAD_LENGTH + ARROW_HEAD_WIDTH / 2.0) / d, sub);
-    Coordinates arrow_head_end = scale((d - node_radius - ARROW_HEAD_LENGTH) / d, sub);
-
-    draw_line_between_coords(renderer, add(scale(node_radius / d, sub), c1), arrow_head_start, color);
-    draw_line_between_coords(renderer,
-                             add(rotate_around(arrow_head_end, arrow_head_width, PI / 2), c1),
-                             arrow_head_start, color);
-    draw_line_between_coords(renderer,
-                             add(rotate_around(arrow_head_end, arrow_head_width, -PI / 2), c1),
-                             arrow_head_start, color);
+void draw_arrow_between_nodes(SDL_Renderer *renderer, const Node *n1, const Node *n2, double node_radius, Uint32 color) {
+    Coordinates vector[2];
+    get_vector(vector, n1, n2, node_radius);
+    draw_arrow(renderer, vector[0], vector[1], color);
 }
 
 /**
- * @brief Draws a line between the to coordinates
+ * @brief Draws a line between the two coordinates
  * 
  * @param renderer The renderer to draw to
  * @param c1 
  * @param c2 
  * @param color Color to draw with
  */
-void draw_line_between_coords(SDL_Renderer *renderer, Coordinates c1, Coordinates c2, Uint32 color) {
+void draw_line(SDL_Renderer *renderer, Coordinates c1, Coordinates c2, Uint32 color) {
     aalineColor(renderer, c1.x, c1.y, c2.x, c2.y, color);
 }
 
 /**
- * @brief Draws the node to the SDL renderer
+ * @brief Draws an arrow
  * 
  * @param renderer 
- * @param n Node to draw
+ * @param from The arrow starts from here
+ * @param to 
+ * @param color 
  */
-void draw_node(SDL_Renderer *renderer, TTF_Font *font, Node *n, double node_radius) {
+void draw_arrow(SDL_Renderer *renderer, Coordinates from, Coordinates to, Uint32 color) {
+    Coordinates sub = subtract(to, from);
+    double d = distance(from, to);
+    assert(d != 0); // we should not divide by zero
+
+    Coordinates arrow_head_width = scale((d - ARROW_HEAD_LENGTH + ARROW_HEAD_WIDTH / 2.0) / d, sub);
+    Coordinates arrow_head_end = scale((d - ARROW_HEAD_LENGTH) / d, sub);
+
+    // draw line connecting the two points
+    draw_line(renderer, from, to, color);
+
+    // draw arrow head on the second point
+    draw_line(renderer,
+              add(rotate_around(arrow_head_end, arrow_head_width, PI / 2), from),
+              to, color);
+    draw_line(renderer,
+              add(rotate_around(arrow_head_end, arrow_head_width, -PI / 2), from),
+              to, color);
+}
+
+/**
+ * @brief Draws a node on the renderer with the specified params
+ * 
+ * @param renderer 
+ * @param font 
+ * @param n 
+ * @param node_radius 
+ */
+void draw_node(SDL_Renderer *renderer, TTF_Font *font, const Node *n, double node_radius) {
     Coordinates pos = normalize_coords(n->coords, SCREEN_WIDTH, SCREEN_HEIGHT);
     filledCircleColor(renderer, pos.x, pos.y, node_radius, NODE_COLOR);
     aacircleColor(renderer, pos.x, pos.y, node_radius, NODE_BORDER_COLOR);
     render_text(renderer, font, TEXT_COLOR, n->name, pos);
+}
+
+/**
+ * @brief Get the vector that connects the two nodes
+ * 
+ * @param vector Has to be a coordinates array with length 2
+ * @param n1 
+ * @param n2 
+ * @param node_radius 
+ */
+void get_vector(Coordinates *vector, const Node *n1, const Node *n2, double node_radius) {
+    Coordinates c1 = normalize_coords(n1->coords, SCREEN_WIDTH, SCREEN_HEIGHT);
+    Coordinates c2 = normalize_coords(n2->coords, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    Coordinates sub = subtract(c2, c1);
+    double d = distance(c1, c2);
+    assert(d != 0); // we should not divide by zero
+
+    vector[0] = add(scale(node_radius / d, sub), c1);
+    vector[1] = add(scale((d - node_radius) / d, sub), c1);
 }
--- src/draw.h
+++ src/draw.h
@@ -6,14 +6,18 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 
-void draw_graph(SDL_Renderer *renderer, TTF_Font *font, Graph *g, Path *exclude_path, double scale_factor);
+void draw_graph(SDL_Renderer *renderer, TTF_Font *font, const Graph *g, const Path *exclude_path);
 
-void draw_line_between_nodes(SDL_Renderer *renderer, Coordinates n1, Coordinates n2, double node_radius, Uint32 color);
+void draw_line_between_nodes(SDL_Renderer *renderer, const Node *n1, const Node *n2, double node_radius, Uint32 color);
 
-void draw_node(SDL_Renderer *renderer, TTF_Font *font, Node *n, double node_radius);
+void draw_arrow_between_nodes(SDL_Renderer *renderer, const Node *n1, const Node *n2, double node_radius, Uint32 color);
 
-void draw_arrow_between_nodes(SDL_Renderer *renderer, Coordinates n1, Coordinates n2, double node_radius, Uint32 color);
+void draw_line(SDL_Renderer *renderer, Coordinates c1, Coordinates c2, Uint32 color);
 
-void draw_line_between_coords(SDL_Renderer *renderer, Coordinates c1, Coordinates c2, Uint32 color);
+void draw_arrow(SDL_Renderer *renderer, Coordinates from, Coordinates to, Uint32 color);
+
+void draw_node(SDL_Renderer *renderer, TTF_Font *font, const Node *n, double node_radius);
+
+void get_vector(Coordinates *vector, const Node *n1, const Node *n2, double node_radius);
 
 #endif
