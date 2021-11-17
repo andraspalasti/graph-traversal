a5d591e4d43c307d9fa4df3e82013e1bed6b1025
andraspalasti <andraspalasti2@gmail.com>
2021-11-17 20:56:40 +0100

    created node hash table

---
+++ src/hash_tables/hash.h
@@ -0,0 +1,6 @@
+#ifndef HASH_H
+#define HASH_H
+
+int hash(const char *key);
+
+#endif
---
+++ src/hash_tables/hash_table_n.h
@@ -0,0 +1,19 @@
+#ifndef HASH_TABLE_N_H
+#define HASH_TABLE_N_H
+
+#include "../node.h"
+#include <stdbool.h>
+
+struct Record;
+
+typedef struct HashTableN {
+    int size;
+    struct Record **records;
+    Node *(*get)(const struct HashTableN *, const char *);
+    void (*set)(struct HashTableN *, char *, Node *);
+    void (*free_table)(struct HashTableN *);
+} HashTableN;
+
+HashTableN *init_hash_table_n(int size);
+
+#endif
